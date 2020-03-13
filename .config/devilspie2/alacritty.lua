bb98c380728a638d1076165e866d98ecae9578e5
natri23 <natri23@gmail.com>
2020-03-13 22:19:02 +0700

    add devilpie2

---
+++ .config/devilspie2/alacritty.lua
@@ -0,0 +1,4 @@
+if (get_application_name()=="Alacritty") then
+    change_workspace(2)
+    set_window_workspace(2)
+    end
---
+++ .config/devilspie2/vm.lua
@@ -0,0 +1,5 @@
+if (get_application_name()=="virt-manager") then
+    set_window_workspace(3);
+    change_workspace(3);
+    set_window_position(100, 100)
+    end
